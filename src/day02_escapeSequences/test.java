2b045f6bf877d30b51ea10340905e250d77c80ad
Ozdemir <ahmet23cemal@gmail.com>
2024-08-15 20:36:25 +0200

    share

---
+++ src/day02_escapeSequences/test.java
@@ -0,0 +1,11 @@
+package day02_escapeSequences;
+
+import utillities.StringUtility;
+
+public class test {
+    public static void main(String[] args) {
+        String word ="JAva LEarnig";
+        String x=StringUtility.reverse(word);
+        System.out.println(x);
+    }
+}
