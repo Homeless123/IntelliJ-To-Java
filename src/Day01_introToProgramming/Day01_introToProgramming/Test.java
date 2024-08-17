d97e61fff3fbafac64a00b0ccb933fedf7fbbb9c
Ozdemir <ahmet23cemal@gmail.com>
2024-08-17 22:35:25 +0200

    https://github.com/Homeless123/StarJava.git

---
+++ src/Day01_introToProgramming/Day01_introToProgramming/Test.java
@@ -0,0 +1,17 @@
+package Day01_introToProgramming.Day01_introToProgramming;
+
+import utillities.StringUtility;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String word ="Wooden Spoon";
+
+        String result = StringUtility.reverse(word);
+
+        System.out.println(result);
+
+
+    }
+
+}
