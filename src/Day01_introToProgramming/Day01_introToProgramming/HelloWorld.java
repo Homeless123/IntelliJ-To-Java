976a9fdcdc0aa7911de24162d64ae15d35c68b03
Ozdemir <ahmet23cemal@gmail.com>
2024-08-17 22:26:25 +0200

    First hello word

---
+++ src/Day01_introToProgramming/Day01_introToProgramming/HelloWorld.java
@@ -0,0 +1,23 @@
+package Day01_introToProgramming.Day01_introToProgramming;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+            //write "main" and enter
+
+        System.out.println("Hello World");
+
+            //write "sout" and enter
+
+        System.out.println("How are you?");
+
+
+
+
+
+    }
+
+
+
+
+}
