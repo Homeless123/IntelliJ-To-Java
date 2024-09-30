880aadecba7681cb6958fee52dc0e7afbde57893
Jomolocco <ahmet23cemal@gmail.com>
2024-09-30 11:37:05 +0200

    new

--- src/day37_exceptions/MultiCatchBlock.java
+++ src/day37_exceptions/MultiCatchBlock.java
@@ -1,8 +1,39 @@
 package day37_exceptions;
 
+import day35_polymorphism.transportationTask.Car;
+
 public class MultiCatchBlock {
 
     public static void main(String[] args) {
+        System.out.println("Program1 Starting");
+
+        Car car = null;
+
+        try {
+            car.drive();
+        } catch (ArithmeticException e ){
+            System.out.println("First Catch block");
+            e.printStackTrace();
+        } catch (ClassCastException e){
+            System.out.println("Second Catch block");
+            e.printStackTrace();
+        }catch (NullPointerException e){
+            System.out.println("Third Catch block");
+            e.printStackTrace();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Fourth Catch block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fifth Catch block");
+            e.printStackTrace();
+        }catch (Exception e) {
+            System.out.println("Default Catch block");
+            e.printStackTrace();
+        }
+
+
+
+        System.out.println("Program1 Ending");
 
 
 
